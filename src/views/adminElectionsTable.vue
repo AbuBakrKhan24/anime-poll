9dee75bbd4d3e62d1fcf0955b0ab5b19a5236bd3
AbuBakrKhan24 <abubakrkhan224@gmail.com@gmail.com>
2022-09-09 00:18:43 +0200

    asdas

--- src/views/adminElectionsTable.vue
+++ src/views/adminElectionsTable.vue
@@ -1,13 +1,170 @@
 <template>
-  
+  <div class="box bg-dark text-light">
+    <div class="header">
+      <h2>List of elections</h2>
+    </div>
+    <div class="row">
+      <div
+        v-for="election in elections"
+        :key="election.elections_id"
+        :election="election"
+        class="card text-dark col-lg-4"
+      >
+        <div class="row">
+          <div class="col-md-4 users_image">
+            <img
+              :src="election.Cover_img"
+              class="img-fluid rounded-start"
+              alt="Cover Image"
+            />
+          </div>
+          <div class="col-md-8 users_details">
+            <div class="card-body">
+              <h5 class="card-title bold">Election's Details</h5>
+              <div class="body_content">
+                <p class="card-text">
+                  <span class="bold"> Title:</span> {{ election.title }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> Election ID:</span>
+                  {{ election.elections_id }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> Category ID:</span>
+                  {{ election.category_ID }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> Election's Description:</span>
+                  {{ election.election_description }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> Creator:</span> {{ election.creator }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> Vote Count:</span>
+                  {{ election.vote_count }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
 export default {
-
-}
+  mounted() {
+    this.$store.dispatch("getElections");
+  },
+  computed: {
+    elections() {
+      console.log(this.$store.state.elections);
+      return this.$store.state.elections;
+    },
+  },
+};
 </script>
+<style scoped>
+.box {
+  padding-top: 83px;
+  min-height: 100vh;
+}
 
-<style>
+.header {
+  padding-top: 3%;
+  padding-bottom: 3%;
+}
 
+img.img-fluid.rounded-start {
+  box-shadow: 3px 4px 15px 5px;
+}
+
+.card {
+  display: flex;
+  width: 40%;
+  height: 350px;
+  margin-right: 4%;
+  margin-bottom: 4%;
+  margin-left: 4%;
+  flex-direction: row;
+  align-items: center;
+}
+
+.card-body {
+  flex: 1 1 auto;
+  padding: var(--bs-card-spacer-y) var(--bs-card-spacer-x);
+  color: var(--bs-card-color);
+  overflow: overlay;
+  height: 300px;
+}
+
+.bold {
+  font-weight: 600;
+}
+
+.body_content {
+  text-align: start;
+}
+
+img.img-fluid.rounded-start {
+  max-height: 226.66px;
+}
+
+.row {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: calc(-1 * var(--bs-gutter-y));
+  margin-right: calc(0 * var(--bs-gutter-x));
+  margin-left: calc(-0.5 * var(--bs-gutter-x));
+  justify-content: center;
+  align-items: center;
+}
+
+@media only screen and (max-width: 1400px) {
+  .card {
+    width: 70%;
+  }
+}
+@media only screen and (max-width: 770px) {
+  .card {
+    display: flex;
+    padding-top: 5%;
+    height: 100%;
+    margin-right: 4%;
+    margin-left: 4%;
+
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .card-body {
+    flex: 1 1 auto;
+    padding: var(--bs-card-spacer-y) var(--bs-card-spacer-x);
+    color: var(--bs-card-color);
+    overflow: overlay;
+    height: 300px;
+    width: 100%;
+  }
+}
+@media only screen and (max-width: 376px) {
+  .card {
+    margin-bottom: 17%;
+  }
+  .card-body {
+    flex: 1 1 auto;
+    padding: var(--bs-card-spacer-y) var(--bs-card-spacer-x);
+    color: var(--bs-card-color);
+    overflow: overlay;
+    height: 300px;
+    width: 112%;
+  }
+  p {
+    margin-top: 0;
+    font-size: small;
+    margin-bottom: 1rem;
+  }
+}
 </style>
--- src/views/adminUsersTable.vue
+++ src/views/adminUsersTable.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="box">
-    <table class="table table-dark table-hover table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">#ID</th>
-          <th scope="col">Username</th>
-          <th scope="col">Email</th>
-          <th scope="col">Password</th>
-          <th scope="col">User Type</th>
-          <th scope="col">Avatar</th>
-          <th scope="col">About</th>
-        </tr>
-      </thead>
-      <tbody id="users">
-        <tr v-for="user in users" :key="user.id" :user="user">
-          <th scope="row">{{ user.id }}</th>
-          <td>{{ user.username }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.password }}</td>
-          <td>{{ user.user_type }}</td>
-          <td>{{ user.avatar }}</td>
-          <td class="td_about">{{ user.about }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="box bg-dark text-light">
+    <div class="header">
+      <h2>List of users</h2>
+    </div>
+    <div class="row">
+      <div
+        v-for="user in users"
+        :key="user.id"
+        :user="user"
+        class="card text-dark col-lg-4 col-md-12"
+      >
+        <div class="row">
+          <div class="col-md-4 users_image">
+            <img
+              :src="user.avatar"
+              class="img-fluid rounded-start"
+              alt="Profile Picture"
+            />
+          </div>
+          <div class="col-md-8 users_details">
+            <div class="card-body">
+              <h5 class="card-title bold">User's Details</h5>
+              <div class="body_content">
+                <p class="card-text">
+                  <span class="bold"> Username:</span> {{ user.username }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> User ID:</span> {{ user.id }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> User Email:</span> {{ user.email }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> User Password:</span> {{ user.password }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> User Type:</span> {{ user.user_type }}
+                </p>
+                <p class="card-text">
+                  <span class="bold"> User's About:</span> {{ user.about }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -42,9 +64,99 @@ export default {
 <style scoped>
 .box {
   padding-top: 83px;
+  min-height: 100vh;
 }
 
-.td_about {
-  height: 65px;
+.header {
+  padding-top: 3%;
+  padding-bottom: 3%;
+}
+
+.card {
+  display: flex;
+  width: 40%;
+  height: 350px;
+  margin-right: 4%;
+  margin-left: 4%;
+  margin-bottom: 4%;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.card-body {
+  flex: 1 1 auto;
+  padding: var(--bs-card-spacer-y) var(--bs-card-spacer-x);
+  color: var(--bs-card-color);
+  overflow: overlay;
+  height: 300px;
+}
+
+.bold {
+  font-weight: 600;
+}
+
+.body_content {
+  text-align: start;
+}
+
+img.img-fluid.rounded-start {
+  box-shadow: 1px 2px 7px 1px;
+}
+
+img.img-fluid.rounded-start {
+  max-height: 226.66px;
+}
+
+.row {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: calc(-1 * var(--bs-gutter-y));
+  margin-right: calc(0 * var(--bs-gutter-x));
+  margin-left: calc(-0.5 * var(--bs-gutter-x));
+  justify-content: center;
+  align-items: center;
+}
+
+@media only screen and (max-width: 1400px) {
+  .card {
+    width: 70%;
+  }
+}
+@media only screen and (max-width: 770px) {
+  .card {
+    display: flex;
+    padding-top: 5%;
+    height: 100%;
+    margin-right: 4%;
+    margin-left: 4%;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .card-body {
+    flex: 1 1 auto;
+    padding: var(--bs-card-spacer-y) var(--bs-card-spacer-x);
+    color: var(--bs-card-color);
+    overflow: overlay;
+    height: 300px;
+    width: 100%;
+  }
+}
+@media only screen and (max-width: 376px) {
+  .card-body {
+    flex: 1 1 auto;
+    padding: var(--bs-card-spacer-y) var(--bs-card-spacer-x);
+    color: var(--bs-card-color);
+    overflow: overlay;
+    height: 300px;
+    width: 112%;
+  }
+  p {
+    margin-top: 0;
+    font-size: small;
+    margin-bottom: 1rem;
+  }
 }
 </style>
